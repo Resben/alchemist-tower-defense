596e6cb055394ce7bbbd9f4c00c30f9fa14b33f6
Resben <bendavidhawkins@gmail.com>
2024-07-21 21:48:13 +0800

    [Feature] Camera controller

--- scenes/levels/main.gd
+++ scenes/levels/main.gd
@@ -1,5 +1,12 @@
 extends Node2D
 
-# side scroll camera on left and right arrow
-func _input(event):
-	pass
+var CAMERA_SPEED = 10
+var HALF_WIDTH = 320
+
+func _physics_process(delta):
+	if Input.is_action_pressed("left"):
+		if $CameraController/Camera2D.limit_left + HALF_WIDTH < $CameraController.global_position.x:
+			$CameraController.global_position.x -= CAMERA_SPEED
+	if Input.is_action_pressed("right"):
+		if $CameraController/Camera2D.limit_right - HALF_WIDTH > $CameraController.global_position.x:
+			$CameraController.global_position.x += CAMERA_SPEED
