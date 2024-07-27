e4bf0c9aef78154ada70565a10da0ec226ae44f0
Resben <bendavidhawkins@gmail.com>
2024-07-27 12:59:03 +0800

    [Feature] attack/defend ui basic

--- scenes/ui/hud.gd
+++ scenes/ui/hud.gd
@@ -1,12 +1,23 @@
 extends CanvasLayer
 
+var attack_active : Texture2D = preload("res://assets/ui/sword.png")
+var attack_inactive : Texture2D = preload("res://assets/ui/sword_inactive.png")
+var defense_active : Texture2D = preload("res://assets/ui/shield-active.png")
+var defense_inactive : Texture2D = preload("res://assets/ui/shield-inactive.png")
+
 func _ready():
+	$Control/HBoxContainer2/Attack.texture_normal = attack_inactive
+	$Control/HBoxContainer2/Defend.texture_normal = defense_active
 	update_items()
 
 func _on_attack_pressed():
+	$Control/HBoxContainer2/Attack.texture_normal = attack_active
+	$Control/HBoxContainer2/Defend.texture_normal = defense_inactive
 	get_parent().on_attack()
 
 func _on_defend_pressed():
+	$Control/HBoxContainer2/Attack.texture_normal = attack_inactive
+	$Control/HBoxContainer2/Defend.texture_normal = defense_active
 	get_parent().on_defend()
 
 func update_items():
