b6ee34725931ea6eb497233a2704e5cafab61bd4
Resben <bendavidhawkins@gmail.com>
2024-08-04 15:22:15 +0800

    [Fix] Error message fix

--- scenes/entities/corpse.gd
+++ scenes/entities/corpse.gd
@@ -1,10 +1,20 @@
 extends Sprite2D
 class_name Corpse
 
-func set_corpse(pos : Vector2, corpse_texture : Texture2D, seconds : int):
+var values_set = false
+var seconds
+
+func _ready():
+	if values_set:
+		$QueueFree.start(seconds)
+	else:
+		push_error("Never set the values on the corpse")
+
+func set_corpse(pos : Vector2, corpse_texture : Texture2D, sec : int):
 	global_position = pos
 	texture = corpse_texture
-	$QueueFree.start(seconds)
+	seconds = sec
+	values_set = true
 
 func _on_queue_free_timeout():
 	var tween = get_tree().create_tween()
